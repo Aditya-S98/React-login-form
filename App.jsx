4569e0c7534fbb3613c6a3570d2551a8496d72e6
Aditya S <aditya.vitv@gmail.com>
2020-11-18 12:32:42 +0530

    login src files first commit

---
+++ App.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+   const [fullName, setFullName] = useState({
+      fname : '',
+      lname : '',
+      email : '',
+      phone : '',
+   });
+
+   const  inputEvent = (event) => {
+
+      console.log(event.target.value);
+      console.log(event.target.name);
+      const {value, name} = event.target;
+
+      setFullName((preValue) => {
+
+         console.log(preValue);
+         return{
+            ...preValue,
+            [name] : value,
+         };
+         // if(name === 'fName') {
+         //    return{
+         //       fname : value,
+         //       lname : preValue.lname,
+         //       email : preValue.email,
+         //       phone : preValue.phone,
+         //    };
+         // } else if(name === 'lName') {
+         //    return{
+         //       fname : preValue.fname,
+         //       lname : value,
+         //       email : preValue.email,
+         //       phone : preValue.phone,
+         //    };
+         // } else if(name === 'email') {
+         //    return{
+         //       fname : preValue.fname,
+         //       lname : preValue.lname,
+         //       email : value,
+         //       phone : preValue.phone,
+         //    };
+         // } else if(name === 'phone') {
+         //    return{
+         //       fname : preValue.fname,
+         //       lname : preValue.lname,
+         //       email : preValue.email,
+         //       phone : value,
+         //    };
+         // }
+      });
+   };
+
+   const onSubmits = (event) => {
+      event.preventDefault();
+   };
+   return(
+      <>
+      <div>
+         <form onSubmit={onSubmits}>
+            <div>
+               <h1>Hello {fullName.fname} {fullName.lname}</h1>
+               <p>{fullName.email}</p>
+               <p>{fullName.phone}</p>
+               <input 
+                  type='text' 
+                  placeholder='Enter your First name' 
+                  name='fname' 
+                  onChange={inputEvent} 
+                  value={fullName.fname}
+               />
+               <input 
+                  type='text' 
+                  placeholder='Enter your Last Name' 
+                  name='lname' 
+                  onChange={inputEvent} 
+                  value={fullName.lname}
+               />
+               <input 
+                  type='email' 
+                  placeholder='Enter your email' 
+                  name='email' 
+                  onChange={inputEvent} 
+                  value={fullName.email}
+               />
+               <input 
+                  type='number' 
+                  placeholder='Enter your Phone number' 
+                  name='phone' 
+                  onChange={inputEvent} 
+                  value={fullName.phone}
+               />
+               <button type='submit'> Submit 😃 </button>
+            </div>
+         </form>
+      </div>
+      </>
+   );
+}
+
+export default App;
